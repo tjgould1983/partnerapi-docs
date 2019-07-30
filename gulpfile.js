6314c470c8e2587fa31797fd1cf41e267cc5541c
nathanm-dev <nathan.marchesi@gmail.com>
2019-07-30 15:51:28 +0100

    Update payload samples for AcceptanceRequested & OrderReadyForPreparation (#79)

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var util = require('gulp-util')
+var util = require('gulp-util');
 var gulpConnect = require('gulp-connect');
 var connect = require('connect');
 var cors = require('cors');
@@ -10,7 +10,31 @@ var swaggerRepo = require('swagger-repo');
 
 var DIST_DIR = 'web_deploy';
 
-gulp.task('serve', ['build', 'watch', 'edit'], function() {
+gulp.task('edit', function() {
+  return portfinder.getPort({port: 5000}, function (err, port) {
+    var app = connect();
+    app.use(swaggerRepo.swaggerEditorMiddleware());
+    app.listen(port);
+    util.log(util.colors.green('swagger-editor started http://localhost:' + port));
+  });
+});
+
+gulp.task('build', function (cb) {
+  return exec('npm run build', function (err, stdout, stderr) {
+    console.log(stderr);
+    cb(err);
+  });
+});
+
+gulp.task('reload', gulp.series('build'), function () {
+  return gulp.src(DIST_DIR).pipe(gulpConnect.reload())
+});
+
+gulp.task('watch', function () {
+  return gulp.watch(['spec/**/*', 'web/**/*'], gulp.series('reload'));
+});
+
+gulp.task('serve-dist', function () {
   portfinder.getPort({port: 3000}, function (err, port) {
     gulpConnect.server({
       root: [DIST_DIR],
@@ -25,26 +49,5 @@ gulp.task('serve', ['build', 'watch', 'edit'], function() {
   });
 });
 
-gulp.task('edit', function() {
-  portfinder.getPort({port: 5000}, function (err, port) {
-    var app = connect();
-    app.use(swaggerRepo.swaggerEditorMiddleware());
-    app.listen(port);
-    util.log(util.colors.green('swagger-editor started http://localhost:' + port));
-  });
-});
-
-gulp.task('build', function (cb) {
-  exec('npm run build', function (err, stdout, stderr) {
-    console.log(stderr);
-    cb(err);
-  });
-});
-
-gulp.task('reload', ['build'], function () {
-  gulp.src(DIST_DIR).pipe(gulpConnect.reload())
-});
-
-gulp.task('watch', function () {
-  gulp.watch(['spec/**/*', 'web/**/*'], ['reload']);
+gulp.task('serve', gulp.parallel('build', 'watch', 'edit', 'serve-dist'), function() {
 });
